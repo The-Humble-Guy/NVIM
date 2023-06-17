092403a2023e72133e1ddcba36d247649208da4b
Alexandr Kucenko <ftruf357ft@gmail.com>
2023-06-18 01:16:36 +0300

    Some changes (add gruvbox theme, remove relative number)

--- init.lua
+++ init.lua
@@ -19,7 +19,8 @@ return {
 
   -- Set colorscheme to use
   colorscheme = "astrodark",
-
+  colorscheme = "gruvbox-material",
+  -- colorscheme = "PaperColor",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,10 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+       -- "andweeb/presence.nvim",
+     "sainnhe/gruvbox-material",
+     "NLKNguyen/papercolor-theme",
+     {"typicode/bg.nvim", lazy = false},
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
