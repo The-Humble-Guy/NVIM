8eed36580595aa0a473c1b2e781878fe08dde9e1
Kutsenko Alexander <ftruf357ft@gmail.com>
2024-01-16 10:15:05 +0300

    Add hotkey to open ToggleTerm in new Tab Enable string wrapping by default

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,7 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>tt"] = { "<cmd>ToggleTerm direction=tab<cr>", desc = "ToggleTerm in new tab" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
