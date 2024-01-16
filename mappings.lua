5fb04fcc729571acb387bf2f1a121aece6fa1e93
Kutsenko Alexander <ftruf357ft@gmail.com>
2024-01-16 20:48:44 +0300

    Redefine <esc> character for terminal mode

--- mappings.lua
+++ mappings.lua
@@ -20,10 +20,11 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    ["<esc>"] = { "<C-\\><C-n>" },
   },
 }
